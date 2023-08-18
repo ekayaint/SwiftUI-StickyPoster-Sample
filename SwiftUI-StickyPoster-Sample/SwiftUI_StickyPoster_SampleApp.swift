68207d512118c3067b632caa46e5ad1816e26b9d
ekayaint <ekayaint@gmail.com>
2023-08-18 12:21:32 +0300

    Initial Commit

---
+++ SwiftUI-StickyPoster-Sample/SwiftUI_StickyPoster_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StickyPoster_SampleApp.swift
+//  SwiftUI-StickyPoster-Sample
+//
+//  Created by ekayaint on 18.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StickyPoster_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
